ff236c41f04b6dd41a43ca6f26a1dcee8f9fbcb4
Boha <imanbekob@gmail.com>
2022-03-02 20:34:39 +0600

    deleted all sout

--- src/main/java/com/digital_nomads/javaFall2021/applications/BankerDemo.java
+++ src/main/java/com/digital_nomads/javaFall2021/applications/BankerDemo.java
@@ -6,16 +6,15 @@ public class BankerDemo {
         DemirBank nursultanDemirBankAccount = new DemirBank(118000001234L, 123456789);
         DemirBank urmatDemirBankAccount = new DemirBank(118000000312L, 987654321);
 
-        System.out.println(nursultanDemirBankAccount.getBalance());
+
         nursultanDemirBankAccount.deposit(5000);
-        System.out.println("After deposit " + nursultanDemirBankAccount.getBalance());
+
 
         transferFunds(nursultanDemirBankAccount, urmatDemirBankAccount, 2_000);
 
-        System.out.println("Urmat's balance after transfer is " + urmatDemirBankAccount.getBalance());
-        System.out.println("Nursultan's balance after transfer is " + nursultanDemirBankAccount.getBalance());
+
 
         transferFunds(urmatDemirBankAccount, nursultanDemirBankAccount, 2101);
-        System.out.println(nursultanDemirBankAccount.getBalance());
+
     }
 }
